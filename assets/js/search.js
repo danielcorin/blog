dbe758814f10bb9306978c639f960c62b1c94bfd
Dan Corin <dcorin6@gmail.com>
2024-07-10 21:54:21 -0400

    Make search nicer

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,15 +1,26 @@
+// modeled off of https://victoria.dev/blog/add-search-to-hugo-static-sites-with-lunr/
+
 function displayResults (results, store) {
-    const searchResults = document.getElementById('results')
+  const searchResults = document.getElementById('search-results')
     if (results.length) {
       let resultList = ''
       // Iterate and build result list elements
       for (const n in results) {
         const item = store[results[n].ref]
-        resultList += '<li role="none"><div><a href="' + item.url + '">' + item.title + '</a></div>'
+        resultList += '<article class="post search-result"><div><a href="' + item.url + '">' + item.title + '</a></div>'
         if (item.date) {
             resultList += '<div class="search-result-date">' + item.date + '</div>'
         }
-        resultList += '<p>' + item.content.substring(0, 150) + '...</p></li>'
+        resultList += '<div>' + item.content.substring(0, 150) + '...</div>'
+        // Add tags to search results
+        if (item.tags && item.tags.length > 0) {
+            resultList += '<div class="search-tags"><ul class="tags">'
+            item.tags.forEach(tag => {
+                resultList += '<li class="' + "tag-" + tag + '"><a href="/tags/' + tag.toLowerCase() + '">' + tag + '</a></li>'
+            })
+            resultList += '</ul></div>'
+        }
+        resultList += '</article>'
       }
       searchResults.innerHTML = resultList
     } else {
@@ -40,8 +51,9 @@ function displayResults (results, store) {
         this.add({
           id: key,
           title: window.store[key].title,
-          tags: window.store[key].category,
-          content: window.store[key].content
+          tags: window.store[key].tags,
+          content: window.store[key].content,
+          date: window.store[key].date,
         })
       }
     })
