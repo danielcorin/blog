7a435a5bdf981d7d26bb78b44c0b55e116eb7638
Dan Corin <dcorin6@gmail.com>
2024-07-19 06:39:29 -0400

    Add search highlighting, reformat log names, refactor post-post

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,65 +1,105 @@
 // modeled off of https://victoria.dev/blog/add-search-to-hugo-static-sites-with-lunr/
 
-function displayResults (results, store) {
-  const searchResults = document.getElementById('search-results')
-    if (results.length) {
-      let resultList = ''
-      // Iterate and build result list elements
-      for (const n in results) {
-        const item = store[results[n].ref]
-        resultList += '<article class="post search-result"><div><a href="' + item.url + '">' + item.title + '</a></div>'
-        if (item.date) {
-            resultList += '<div class="search-result-date">' + item.date + '</div>'
-        }
-        resultList += '<div>' + item.content.substring(0, 150) + '...</div>'
-        // Add tags to search results
-        if (item.tags && item.tags.length > 0) {
-            resultList += '<div class="search-tags"><ul class="tags">'
-            item.tags.forEach(tag => {
-                resultList += '<li class="' + "tag-" + tag + '"><a href="/tags/' + tag.toLowerCase() + '">' + tag + '</a></li>'
-            })
-            resultList += '</ul></div>'
-        }
-        resultList += '</article>'
-      }
-      searchResults.innerHTML = resultList
-    } else {
-      searchResults.innerHTML = 'No results found.'
-    }
+function displayResults(results, store) {
+  const searchResults = document.getElementById('search-results');
+
+  if (results.length === 0) {
+    searchResults.innerHTML = 'No results found.';
+    return;
   }
 
-  // Get the query parameter(s)
-  const params = new URLSearchParams(window.location.search)
-  const query = params.get('query')
+  const resultList = results.map(result => {
+    const item = store[result.ref];
+    const highlightedTitle = highlightText(item.title, query);
+    const contentSnippet = getContentSnippet(item.content, query);
+    const highlightedContent = highlightText(contentSnippet, query);
+    return `
+      <article class="post search-result">
+        <div><a href="${item.url}">${highlightedTitle}</a></div>
+        ${item.date ? `<div class="search-result-date">${item.date}</div>` : ''}
+        <div>${highlightedContent}</div>
+        ${renderTags(item.tags)}
+      </article>
+    `;
+  }).join('');
 
-  // Perform a search if there is a query
-  if (query) {
-    // Retain the search input in the form when displaying results
-    document.getElementById('search-input').setAttribute('value', query)
+  searchResults.innerHTML = resultList;
+}
 
-    const idx = lunr(function () {
-      this.ref('id')
-      this.field('title', {
-        boost: 15
-      })
-      this.field('tags')
-      this.field('content', {
-        boost: 10
-      })
+function getContentSnippet(content, query) {
+  if (content.length <= 150) {
+    return content;
+  }
 
-      for (const key in window.store) {
-        this.add({
-          id: key,
-          title: window.store[key].title,
-          tags: window.store[key].tags,
-          content: window.store[key].content,
-          date: window.store[key].date,
-        })
-      }
+  const words = query.split(/\s+/).filter(word => word.length > 0);
+  const regex = new RegExp(words.join('|'), 'gi');
+  const match = content.match(regex);
+
+  if (match) {
+    const matchIndex = content.indexOf(match[0]);
+    const startIndex = Math.max(0, content.lastIndexOf(' ', matchIndex - 75));
+    const endIndex = Math.min(content.length, content.indexOf(' ', matchIndex + 75));
+    let snippet = content.substring(startIndex, endIndex);
+
+    if (startIndex > 0) snippet = '...' + snippet.trimStart();
+    if (endIndex < content.length) snippet = snippet.trimEnd() + '...';
+
+    return snippet;
+  }
+}
+
+function highlightText(text, query) {
+  const words = query.split(/\s+/).filter(word => word.length > 0);
+  let highlightedText = text;
+  words.forEach(word => {
+    const regex = new RegExp(`\\b(${word})`, 'gi');
+    highlightedText = highlightedText.replace(regex, '<span class="search-highlight">$1</span>');
+  });
+  return highlightedText;
+}
+
+function renderTags(tags) {
+  if (!tags || tags.length === 0) return '<div class="search-tags" style="margin-bottom: 1em;"></div>';
+
+  const tagList = tags.map(tag =>
+    `<li class="tag-${tag}"><a href="/tags/${tag.toLowerCase()}">${tag}</a></li>`
+  ).join('');
+
+  return `<div class="search-tags"><ul class="tags">${tagList}</ul></div>`;
+}
+
+// Get the query parameter(s)
+const params = new URLSearchParams(window.location.search)
+const query = params.get('query')
+
+// Perform a search if there is a query
+if (query) {
+  // Retain the search input in the form when displaying results
+  document.getElementById('search-input').setAttribute('value', query)
+
+  const idx = lunr(function () {
+    this.ref('id')
+    this.field('title', {
+      boost: 15
+    })
+    this.field('tags')
+    this.field('content', {
+      boost: 10
     })
 
-    // Perform the search
-    const results = idx.search(query)
-    // Update the list with results
-    displayResults(results, window.store)
-  }
+    for (const key in window.store) {
+      this.add({
+        id: key,
+        title: window.store[key].title,
+        tags: window.store[key].tags,
+        content: window.store[key].content,
+        date: window.store[key].date,
+      })
+    }
+  })
+
+  // Perform the search
+  const results = idx.search(query)
+  // Update the list with results
+  displayResults(results, window.store)
+}
